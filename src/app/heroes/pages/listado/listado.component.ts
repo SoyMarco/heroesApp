d749447146338a7dcfd8fbeba5282437386a12b9
Marco Antonio Salazar Ramirez <68885812+SoyMarco@users.noreply.github.com>
2022-06-21 22:31:32 -0500

    Curso Angular Udemy

---
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interfaces';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent implements OnInit {
+  heroes: Heroe[] = [];
+  constructor(private heroesService: HeroesService) {}
+
+  ngOnInit(): void {
+    this.heroesService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroes = heroes));
+  }
+}
